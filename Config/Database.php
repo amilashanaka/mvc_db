7401c3062dec10756406eb157e95a4f9fb3ec6e0
amilashanaka <dsa.amilashanaka@gmail.com>
2022-11-26 18:06:26 +0000

    update

--- Config/Database.php
+++ Config/Database.php
@@ -1,6 +1,7 @@
 <?php
 
 namespace Config;
+use PDO;
 
 class Database {
     public static $host = 'localhost';
@@ -8,8 +9,13 @@ class Database {
     public static  $db_user = 'root';
     public static $pass = '';
 
-    private static function connect(){
+    public  function connect(){
 
         $pdo=new PDO("mysql:host=".self::$host.";dbname=".self::$db.";charset=utf8".self::$db_user.",".self::$pass);
+
+        return $pdo;
     }
+
+
+
 }
--- index.php
+++ index.php
@@ -2,3 +2,9 @@
 //include_once("controller/UserController.php");
 //$controller = new UserController();
 //$controller->invoke();
+
+include_once 'Config/Database.php';
+
+ $db=new \Config\Database();
+
+ var_dump($db->connect());
